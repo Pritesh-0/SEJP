2243e43a010643e198350c8fba4ab7ee88a42963
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-04-16 15:54:02 +0530

    Implement password hashing and JWT Bearer auth

--- sejp/main.py
+++ sejp/main.py
@@ -1,9 +1,12 @@
+from datetime import datetime, timedelta
 from fastapi import FastAPI, Response, status, Depends, HTTPException
 from fastapi.encoders import jsonable_encoder
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from typing import Optional, Annotated
 from pydantic import BaseModel, Field
 from pymongo import MongoClient
+from passlib.context import CryptContext
+from jose import JWTError, jwt
 from dotenv import dotenv_values
 import ulid
 
@@ -46,6 +49,7 @@ class User(BaseModel):
 @app.post("/user", response_description="Create a new user", 
           status_code=status.HTTP_201_CREATED, response_model=User)
 def create_user(new_user: User):
+    new_user.password = get_password_hash(new_user.password)
     new_user_ = jsonable_encoder(new_user)
     new_user_ = app.database["users"].insert_one(new_user_)
     created_user = app.database["users"].find_one(
@@ -77,29 +81,46 @@ def create_job():
 @app.put("/job/{job_id}")
 def modify_job():
     pass
-# fake_users_db = {
-#     "johndoe": {
-#         "username": "johndoe",
-#         "full_name": "John Doe",
-#         "email": "johndoe@example.com",
-#         "hashed_password": "fakehashedsecret",
-#         "disabled": False,
-#     },
-#     "alice": {
-#         "username": "alice",
-#         "full_name": "Alice Wonderson",
-#         "email": "alice@example.com",
-#         "hashed_password": "fakehashedsecret2",
-#         "disabled": True,
-#     },
-# }
 
-class UserInDB():
-    hashed_password: str
-def fake_hash_password(s):
-    return s
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-@app.post("/token")
+
+def get_user(email: str):
+    return User(**app.database["users"].find_one({
+        "email": email
+    }))
+def authenticate_user(email: str, password: str):
+    user = get_user(email)
+    if not user:
+        return False
+    if not verify_password(password, user.password):
+        return False
+    return user
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+SECRET_KEY = config["SECRET_KEY"]
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+@app.post("/token", response_model=Token)
 def auth(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user_dict = app.database["users"].find_one({
         "email": form_data.username
@@ -118,13 +139,21 @@ def fake_decode_token(token):
     })
     return user
 def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid authentication credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username)
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+        raise credentials_exception
     return user
 def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)]):
     if current_user.disabled:
