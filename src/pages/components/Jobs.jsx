14310e54c2cff8d8901e273efb561d399beb7910
Pritesh-0 <priteshagrawal0@gmail.com>
2023-04-17 13:44:52 +0530

    added job card

--- src/pages/components/Jobs.jsx
+++ src/pages/components/Jobs.jsx
@@ -4,6 +4,7 @@ export default function Home(){
         <div className='grid grid-rows-3 grid-cols-4 grid-flow-col h-screen'>
         <div className='col-start-2 col-span-3 row-span-3 bg-gray-900 mx-32 my-20 rounded-3xl'>
         <Card/>
+        {/* <Card/> */}
         </div>
       </div>
     )
