ba43678d9cb769b773c4a0d583cf2496b3c4c089
Pritesh-0 <priteshagrawal0@gmail.com>
2023-04-17 17:57:52 +0530

    login page finished

--- src/pages/components/Login.jsx
+++ src/pages/components/Login.jsx
@@ -3,23 +3,27 @@ import { AiOutlineMail, AiFillLock } from 'react-icons/ai'
 import { FaUser } from 'react-icons/fa'
 export default function Home(){
     return(
-        <div className='h-screen grid grid-rows-8 grid-cols-8 m-auto'>
-            <div className="bg-gray-700 row-start-3 col-start-4 row-span-3 col-span-2 flex flex-col text-xl rounded-3xl ">
-                <div><CiUser className='text-9xl bg-white text-gray-950 rounded-full mx-auto mt-12'/></div>
+        <div className='h-screen grid grid-rows-4 grid-cols-8 m-auto'>
+            <div className="bg-gray-700 row-start-2 col-start-4 col-span-2 row-span-4 flex flex-col text-xl rounded-3xl mb-36">
+                <div><CiUser className='text-9xl bg-white text-gray-950 rounded-full mx-auto mt-20  '/></div>
                 <form className='mx-auto mt-10'>
                 <div className='flex'>
                     <AiOutlineMail/>
-                    <input placeholder='Email ID' id='email' type='email' required className='bg-transparent ml-6'></input>
+                    <input placeholder='Email ID' id='email' type='email' required className='bg-transparent ml-6 '></input>
                 </div>         
-                <hr className='my-3'></hr>
+                <div className='bg-white h-px my-3'></div>
                 <div className='flex'>
                     <AiFillLock/>
                     <input placeholder='Password' id='Password' type='password' required className='bg-transparent ml-6'></input>
                 </div>
-                <hr className='my-3'></hr>
+                <div className='bg-white h-px my-3'></div>
                 <button className='bg-gray-950 px-10 py-1.5 rounded-md mt-4 '>Login</button>
-                <hr className='bg-black '></hr>
+                
                 </form>
+                <div className='bg-gray-950 my-3 h-3 w-full'></div>
+                <div className='text-3xl text-center text-gray-400 mt-1 cursor-pointer'>
+                    REGISTER
+                </div>
             </div>
         </div>
     )
