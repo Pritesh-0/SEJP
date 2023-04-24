4204e2b4e71168f44bf89123285c7d095d0cf6e4
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-04-24 19:22:26 +0530

    Handle basic searching

--- src/pages/Jobpage.jsx
+++ src/pages/Jobpage.jsx
@@ -1,8 +1,26 @@
 import { AiOutlineSearch } from 'react-icons/ai'
 import Link from 'next/link';
 import Card from '/src/pages/components/Card.jsx'
+import { useState } from 'react';
 
 export default function Home() {
+    const handleSearch = async(event) => {
+        event.preventDefault()
+        const data = {
+            // position: event.target.position.value.toString(),
+            employer: event.target.company.value.toString(),
+            // required_skills: []
+        }
+        const formData = JSON.stringify(data)
+        console.log(formData)
+        const response = await fetch("http://localhost:5000/search?matches=10",
+        {method: "POST", headers: {"accept": "application/json", "Content-Type": "application/json"}, body: formData})
+        const responseCode = await response.status
+        const body = await response.json()
+        console.log(body);
+        setResults(body);
+    }
+    const [results, setResults]= useState([]);
     return (
         <div className="">
             <nav className='py-12 px-12 flex justify-between relative'>
@@ -19,15 +37,15 @@ export default function Home() {
             </nav>
             <div className='bg-gray-950'>
             <div className='bg-gray-800 h-px my-3'></div>
-                <form className='flex ml-10'>
+                <form className='flex ml-10' onSubmit={handleSearch}>
                 <div className='flex'>
                     <AiOutlineSearch className=' text-xl mr-3 mt-1'/>
                     <h1 className='mr-3 mt-1'>Search by:</h1>
-                    <input placeholder='Position' id='position' type='text' name='position' autoComplete='off' className='bg-transparent mx-4'></input>
-                    <input placeholder='Skills' id='skills' type='text' name='skills' autoComplete='off' className='bg-transparent mx-4'></input>
-                    <input placeholder='Company' id='company' type='text' name='company' autoComplete='off' className='bg-transparent mx-4 '></input>
+                    <input placeholder='Position' id='position' type='text' name='position' className='bg-transparent mx-4'></input>
+                    <input placeholder='Skills' id='skills' type='text' name='skills' className='bg-transparent mx-4'></input>
+                    <input placeholder='Company' id='company' type='text' name='company' className='bg-transparent mx-4 '></input>
                 </div> 
-                <button className='bg-black px-10 py-1.5 rounded-md '>SEARCH</button>
+                <button className='bg-black px-10 py-1.5 rounded-md ' on>SEARCH</button>
                 
                 </form>
                 <div className='bg-gray-800 h-px my-3'></div>
@@ -41,7 +59,9 @@ export default function Home() {
             </div>
             <div className='bg-gray-800 h-px my-3'></div> */}
             <div className='ml-72'>
-                <Card/>
+                {results.map(function(object, id) {
+                    return <Card/>;
+                })}
             </div>
 
 
